0537d139b1d13702f911727cb6fb88ae6a8a13ed
ArsVazhnov <88939449+ArsVazhnov@users.noreply.github.com>
2026-08-24 13:41:26 +0300

    Project files upload

---
+++ Source/VRDev_Vazhnov/VRDev_Vazhnov.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRDev_Vazhnov.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRDev_Vazhnov, "VRDev_Vazhnov" );
