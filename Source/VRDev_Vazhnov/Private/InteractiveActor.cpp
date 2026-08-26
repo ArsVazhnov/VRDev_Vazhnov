d4bbff7a76da02c10fab91c5b05099ce31d6aa45
ArsVazhnov <88939449+ArsVazhnov@users.noreply.github.com>
2026-08-26 22:37:52 +0300

    v0.3 * В InteractiveActor добавлены все необходимые типы данных, а также перенесены методы выделения и захвата объекта; * Данные из InteractiveActor теперь отображаются в VR UI при выделении или захвате этого объекта.

--- Source/VRDev_Vazhnov/Private/InteractiveActor.cpp
+++ Source/VRDev_Vazhnov/Private/InteractiveActor.cpp
@@ -1,5 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "InteractiveActor.h"
 
+AInteractiveActor::AInteractiveActor()
+{
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaterialAsset(TEXT("/Script/Engine.Material'/Game/Materials/M_HoveredObjectOverlay.M_HoveredObjectOverlay'"));
+	HoveredOverlayMaterial = MaterialAsset.Object;
+	
+	UStaticMeshComponent* MeshComp = GetStaticMeshComponent();
+	MeshComp->Mobility = EComponentMobility::Movable;
+	MeshComp->SetSimulatePhysics(true);
+	MeshComp->SetCollisionProfileName(TEXT("PhysicsActor"));
+}
+
+void AInteractiveActor::SetIsHovered(const bool bIsHovered) const
+{
+	UStaticMeshComponent* MeshComp = GetStaticMeshComponent();
+	MeshComp->SetOverlayMaterial(bIsHovered && HoveredOverlayMaterial ? HoveredOverlayMaterial : nullptr);
+}
+
+void AInteractiveActor::SetIsHeld(const bool bIsHeld) const
+{
+	UStaticMeshComponent* MeshComp = GetStaticMeshComponent();
+	MeshComp->SetSimulatePhysics(!bIsHeld);
+	MeshComp->SetCollisionProfileName(bIsHeld ? TEXT("NoCollision") : TEXT("PhysicsActor"));
+}
--- Source/VRDev_Vazhnov/Public/InteractiveActor.h
+++ Source/VRDev_Vazhnov/Public/InteractiveActor.h
@@ -1,17 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/StaticMeshActor.h"
 #include "InteractiveActor.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum class EInteractiveActorState : uint8
+{
+	Hovered UMETA(DisplayName = "Hovered"),
+	Held UMETA(DisplayName = "Held")
+};
+
+USTRUCT(BlueprintType)
+struct FInteractiveActorData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) FString Type;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ClampMin = 0, UIMin = 0)) float Weight;
+};
+
 UCLASS()
 class VRDEV_VAZHNOV_API AInteractiveActor : public AStaticMeshActor
 {
 	GENERATED_BODY()
 	
+public:
+	AInteractiveActor();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) FInteractiveActorData Data;
+	
+	UFUNCTION(BlueprintCallable) void SetIsHovered(bool bIsHovered) const;
+	UFUNCTION(BlueprintCallable) void SetIsHeld(bool bIsHeld) const;
+	
+private:
+	UPROPERTY() UMaterialInterface* HoveredOverlayMaterial;
 };
